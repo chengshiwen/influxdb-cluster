b8dea833b275b2864017e0e9c4b959eaa866e4d1
davidby-influx <72418212+davidby-influx@users.noreply.github.com>
2024-06-24 10:24:34 -0700

    fix: GROUP BY queries with offset that crosses a DST boundary fail. (#25082)
    This is actually the second fix for
    https://github.com/influxdata/influxdb/issues/20238
    for when the time zone falls back in autumn.
    
    closes https://github.com/influxdata/influxdb/issues/25078

--- query/iterator.go
+++ query/iterator.go
@@ -847,6 +847,10 @@ func (opt IteratorOptions) Window(t int64) (start, end int64) {
 		end = t + dt
 	}
 
+	// As above, the offset has to happen before the time zone calculation.
+	// This is another fix for https://github.com/influxdata/influxdb/issues/20238
+	// that was missed the first time.
+	end += int64(opt.Interval.Offset)
 	// Retrieve the zone offset for the end time.
 	if opt.Location != nil {
 		_, endOffset := opt.Zone(end)
@@ -872,7 +876,6 @@ func (opt IteratorOptions) Window(t int64) (start, end int64) {
 			}
 		}
 	}
-	end += int64(opt.Interval.Offset)
 	return
 }
 
